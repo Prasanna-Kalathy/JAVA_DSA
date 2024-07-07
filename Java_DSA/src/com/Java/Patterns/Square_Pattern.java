d096048e951ce0a0ae52609ed90887c29f5890a6
Prasanna_Kalathy <Prasannakalathy@gmail.com>
2024-07-07 18:32:29 +0530

    Right Triangle Continues Number Patterns

--- Java_DSA/src/com/Java/Patterns/Square_Pattern.java
+++ Java_DSA/src/com/Java/Patterns/Square_Pattern.java
@@ -8,11 +8,13 @@ public class Square_Pattern {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter the Number: ");
 		int Num = input.nextInt();
+
+//		Since the value increases continuously
+		int val = 1;
 		for (int i = 1; i <= Num; i++) {
-//			for (int j = 0; j <= Num-i; j++) {
-//			for (int j = Num-1; j >= Num-i; j--) {
 			for (int j = 1; j <= i; j++) {
-				System.out.print("*");
+				System.out.print(val);
+				val++;
 			}
 			System.out.println();
 		}
