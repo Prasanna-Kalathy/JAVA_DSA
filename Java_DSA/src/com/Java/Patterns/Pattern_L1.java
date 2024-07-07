5fb2200128386d80713762085d555b522aa02dfd
Prasanna_Kalathy <Prasannakalathy@gmail.com>
2024-07-07 20:02:32 +0530

    Reverse Right and Numbers

--- Java_DSA/src/com/Java/Patterns/Pattern_L1.java
+++ Java_DSA/src/com/Java/Patterns/Pattern_L1.java
@@ -9,11 +9,39 @@ public class Pattern_L1 {
 		System.out.println("Enter the Number: ");
 		int Num = input.nextInt();
 
-		Pattern2(Num);
+		Pattern5(Num);
 
 		input.close();
 	}
 
+
+
+
+
+
+
+
+	
+	//Reverse Right Triangle Pattern
+	public static void Pattern5(int Num) {
+		for (int i = 1; i <= Num; i++) {
+			for (int j = 0; j <= Num-i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	// Row and Column Number Pattern
+	public static void Pattern4(int Num) {
+		// Since the value increases continuously
+		for (int i = 1; i <= Num; i++) {
+			for (int j = 1; j <= i; j++) {
+				// System.out.print(i);
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+	}
 	// Continues_Pattern
 	public static void Pattern3(int Num) {
 		// Since the value increases continuously
