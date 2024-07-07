e9f703ecfda70e295e3d280426107f689a578ee9
Prasanna_Kalathy <Prasannakalathy@gmail.com>
2024-07-07 22:25:54 +0530

    Dimond Pattern

--- Java_DSA/src/com/Java/Patterns/Pattern_L2.java
+++ Java_DSA/src/com/Java/Patterns/Pattern_L2.java
@@ -7,11 +7,28 @@ public class Pattern_L2 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter the Number of Rows: ");
-		int Row = input.nextInt();
-		
-		Pattern1(Row);
+		// int Row = input.nextInt();
+
+		Pattern1(10);
+		Pattern2(10);
 
 		input.close();
+	}
+
+	// Pyramid Upside down Pattern
+	public static void Pattern2(int Row) {
+		for (int line = 1; line <= Row; line++) {
+			for (int Space = 1; Space <= (line - 1); Space++) {
+				System.out.print(" ");
+			}
+			for (int Star = 1; Star <= ((Row - line) * 2 + 1); Star++) {
+				System.out.print("*");
+			}
+			for (int Space = 1; Space <= (line - 1); Space++) {
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
 	}
 
 	// Pyramid Pattern
