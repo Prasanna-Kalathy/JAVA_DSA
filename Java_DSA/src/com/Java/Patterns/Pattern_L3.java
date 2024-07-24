d3116219cead64a5f6c54a83c55c2075fc25d021
Prasanna_Kalathy <Prasannakalathy@gmail.com>
2024-07-24 23:50:48 +0530

    ABA Pyramid pattern

---
+++ Java_DSA/src/com/Java/Patterns/Pattern_L3.java
@@ -0,0 +1,33 @@
+package com.Java.Patterns;
+
+import java.util.Scanner;
+
+public class Pattern_L3 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		// System.out.println("Enter the Number of Rows: ");
+		// int Row = input.nextInt();
+		Pattern1(4);
+		input.close();
+	}
+
+	// Pyramid Pattern
+	public static void Pattern1(int Row) {
+		for (int i = 1; i <= Row; i++) {
+			for (int Space = Row - i; Space >= 1; Space--) {
+				System.out.print(" ");
+			}
+			for (char j = 'A'; j < (i - 1) + 'A'; j++) {
+				System.out.print(j);
+			}
+			for (char k = (char) ((i - 1) + 'A'); k >= 'A'; k--) {
+				System.out.print(k);
+			}
+			for (int Space = 1; Space <= Row - i; Space++) {
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+}
