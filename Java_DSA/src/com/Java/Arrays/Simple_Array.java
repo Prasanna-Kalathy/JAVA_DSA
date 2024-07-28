1da5ea91d7c76c6f47a1e184def94a492eb1df50
Prasanna_Kalathy <Prasannakalathy@gmail.com>
2024-07-28 23:55:34 +0530

    A Simple Array of Sum

---
+++ Java_DSA/src/com/Java/Arrays/Simple_Array.java
@@ -0,0 +1,20 @@
+package com.Java.Arrays;
+
+public class Simple_Array {
+
+	public static int SumArr(int[] arr) {
+		int sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			sum += arr[i];
+		}
+		System.out.println(sum);
+		return sum;
+
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		SumArr(arr);
+	}
+
+}
