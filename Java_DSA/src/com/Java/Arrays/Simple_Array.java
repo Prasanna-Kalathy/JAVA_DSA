c830d55bf2df6c3022b0ac71b50bc906af2f8ca6
Prasanna_Kalathy <Prasannakalathy@gmail.com>
2024-07-29 20:14:07 +0530

    Minor Changes

--- Java_DSA/src/com/Java/Arrays/Simple_Array.java
+++ Java_DSA/src/com/Java/Arrays/Simple_Array.java
@@ -9,12 +9,10 @@ public class Simple_Array {
 		}
 		System.out.println(sum);
 		return sum;
-
 	}
 
 	public static void main(String[] args) {
 		int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 		SumArr(arr);
 	}
-
 }
