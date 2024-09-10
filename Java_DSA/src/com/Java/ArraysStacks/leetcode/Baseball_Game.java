e91d13597b5eea094bdfb3c85b169074ec3d909e
Prasanna_Kalathy <Prasannakalathy@gmail.com>
2024-09-11 00:48:56 +0530

    Baseball Game Perfected to 0ms

--- Java_DSA/src/com/Java/ArraysStacks/leetcode/Baseball_Game.java
+++ Java_DSA/src/com/Java/ArraysStacks/leetcode/Baseball_Game.java
@@ -1,47 +1,45 @@
 package com.Java.ArraysStacks.leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Baseball_Game {
 
 	public static void main(String[] args) {
-		String operations[] = { "5","-2","4","C","D","9","+","+" };
-		for (int i = 0; i < operations.length; i++) {
-			System.out.print(operations[i] + " ");
+		String operations[] = { "5", "-2", "4", "C", "D", "9", "+", "+" };
+		for(String s: operations){
+			System.out.print(s + " ");
 		}
 		System.out.println();
+
+		int ans[] = new int[operations.length];
+		int index = 0; //Implementing the index feature of Arraylist for the Array
 		
-		List<Integer> ans = new ArrayList<>();
-		for (int i = 0; i < operations.length; i++) {
-			String str =operations[i];
-			if(Character.isDigit(str.charAt(0)) || str.charAt(0)=='-') {
-				ans.add(Integer.parseInt(str));
-				System.out.println("Added "+str);
-			}
-			else {
-				switch (str) {
-				case "C":
-					System.out.println("Removed "+ans.getLast());
-					ans.removeLast();
-					break;
-				case "D":
-					ans.addLast(ans.getLast()*2);
-					System.out.println("Added "+ans.getLast());
-					
-					break;
-				case "+":
-					System.out.println("Added "+ans.getLast()+" and "+(ans.get(ans.size()-2)));
-					ans.add(ans.getLast()+(ans.get(ans.size()-2)));
-					break;
-				}
+		for(String str: operations){
+			switch (str) {
+			case "C":
+				index--;
+				System.out.println("Removed " + str);
+				break;
+			case "D":
+				ans[index] = ans[index-1]*2;
+				index++;
+				System.out.println("Added " + ans[index]);
+				break;
+			case "+":
+				ans[index] = ans[index-1] + ans[index-2];
+				System.out.println("Added " + ans[index]);
+				index++;
+				break;
+			default:
+				ans[index] = Integer.parseInt(str);
+				index++;
+				break;
 			}
 		}
-		System.out.println(ans);
 		int sum = 0;
 		for (int point : ans) {
-            sum += point;
-        }
+			sum += point;
+		}
 		System.out.println(sum);
+		for(int i: ans)
+		System.out.print(i+ " ");
 	}
 }
