0df352fbf982d9d78ad8b93712c2d2ca2e46c335
Prasanna_Kalathy <Prasannakalathy@gmail.com>
2024-09-12 23:19:34 +0530

    Min Stack - Need to brushup Linkedlist or List to Stack

---
+++ Java_DSA/src/com/Java/ArraysStacks/leetcode/MinStack.java
@@ -0,0 +1,38 @@
+package com.Java.ArraysStacks.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MinStack {
+	
+	public static void main(String[] args) {
+		int val =5;
+		MinStack obj = new MinStack();
+		obj.push(val);
+		System.out.println(obj);
+		obj.pop();
+//		int param_3 = obj.top();
+//		int param_4 = obj.getMin();
+	}
+
+	public MinStack() {
+		List<Integer> MinStk = new ArrayList<>();
+	}
+
+	public void push(int val) {
+		
+	}
+
+	public void pop() {
+		
+	}
+
+//	public int top() {
+//
+//	}
+//
+//	public int getMin() {
+//
+//	}
+
+}
